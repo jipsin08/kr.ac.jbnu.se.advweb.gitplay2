811dc1d961bb44089a8aad3be52046e05697508d
User <User@DESKTOP-4GJLF04>
2017-09-19 15:14:29 +0900

    hello

--- src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit.java
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit.java
@@ -6,7 +6,7 @@ public class HelloGit {
 		System.out.println("Hello Git!!");
 		System.out.println("Hello Git!!???????????");
 		
-
+		// Hello, git!
 		for(int i = 0;i < 100; i++)
 		{
 			System.out.println("i:" + i);
